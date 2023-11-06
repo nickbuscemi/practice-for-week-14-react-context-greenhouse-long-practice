fc3b1e02f8e352d3275fd8c92205d19bcdeb9e77
Nick <nick.buscemi13@gmail.com>
2023-11-06 12:20:45 -0500

    pt.1 complete

--- src/components/Greenhouse/Greenhouse.js
+++ src/components/Greenhouse/Greenhouse.js
@@ -4,13 +4,17 @@ import './Greenhouse.css';
 
 import LightSwitch from './LightSwitch';
 import ClimateStats from './ClimateStats';
+import { useTheme } from '../../context/ThemeContext.js'
 
 function Greenhouse() {
 
-  return (
+  const { themeName } = useTheme();
+  // CHooose the image based on the theme name
+  const image = themeName === 'day' ? dayImage : nightImage;
+   return (
     <section>
       <img  className='greenhouse-img'
-            src={dayImage}
+            src={image}
             alt='greenhouse' 
       />
       <LightSwitch />
